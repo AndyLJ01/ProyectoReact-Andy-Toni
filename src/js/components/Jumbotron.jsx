10df02a1be554ebb871066c5dbd2995b74d82338
AndyLJ01 <andylinjl@gmail.com>
2026-02-26 22:57:19 +0100

    Jumbotron.jsx

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Jumbotron() {
+  return (
+    <div className="bg-body-secondary mt-4">
+            <div className="container-fluid text-start py-4">
+                <h1 className="fw-medium display-2 py-4 ms-5">A Warm Welcome!</h1>
+                <p className="fs-5 py-2 ms-5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat.
+                </p>
+                <button className="btn btn-primary btn-lg ms-5 mb-5" type="button">
+                    Call to action!
+                </button>
+            </div>
+        </div>
+  )
+}
+
+export default Jumbotron
