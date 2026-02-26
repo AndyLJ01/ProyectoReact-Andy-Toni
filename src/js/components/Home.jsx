1ca346b75174188cedccffbbfbc09d03165e2e12
AndyLJ01 <andylinjl@gmail.com>
2026-02-26 22:58:45 +0100

    Cambio del Home para incluir el Jumbotron

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
@@ -8,6 +9,16 @@ const Home = () => {
 	return (
 		<div className="text-center">
 			<Navbar/>
+     	<div className="container">
+            <Jumbotron/>
+
+
+
+
+
+
+
+		</div>
 		</div>
 	);
 };
